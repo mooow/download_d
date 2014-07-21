6c5a73e32fa6863e6a7e5a5ce3039aace4f08885
mooow <mureulor@gmail.com>
2014-07-21 17:03:44 +0200

    Added GPLv2 copying notice, and minor changes
    *) Added GPLv2 copying notice
    *) A message is displayed which tells how many images have to be
    downloaded

--- download_d.py
+++ download_d.py
@@ -1,5 +1,23 @@
-import os, lxml.html, urllib.request as request
+# download_d Copyright © 2014 Lorenzo Mureu <mureulor@gmail.com>
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# 
 
+print("download_d Copyright © 2014 Lorenzo Mureu <mureulor@gmail.com>\n\n\n")
+
+import os, lxml.html, urllib.request as request
 
 pdir = os.getenv("HOME") or os.getenv("HOMEPATH")
 print("HOME: " + pdir)
@@ -14,7 +32,10 @@ o = request.urlopen(URL_IMAGES)
 # load DOM
 doc = lxml.html.document_fromstring(o.readall().decode())
 # search all <img> tags
-for img in doc.findall(".//img"):
+imgs = doc.findall(".//img");
+
+print("{0} images found".format(len(imgs)))
+for img in imgs:
     url = URL_BASE + img.attrib["src"] # img url
     bn = img.attrib["id"]              # file name
     fp = os.path.join(ddir, bn)        # file path
